d3c505ce2b524eb45e5f3c244064fbc1621f7a38
abhijith.pm <abhijith.pm@fingent.com>
2021-01-11 19:39:28 +0530

    CollectionView cells Defined

--- iSpeedo/Screens/CreateRide/CollectionViewCells/RideDataCell.swift
+++ iSpeedo/Screens/CreateRide/CollectionViewCells/RideDataCell.swift
@@ -9,16 +9,46 @@ import UIKit
 
 class RideDataCell: UICollectionViewCell {
     
-//    var 
+    var dataLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "999 KM/Hr"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        return label
+    }()
+    
+    var titleLabel: UILabel = {
+        let label: UILabel = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Average Speed"
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 20)
+        return label
+    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        initialiseViews()
     }
     
     required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: coder)
+        initialiseViews()
     }
     func initialiseViews() {
         
+        contentView.addSubview(dataLabel)
+        dataLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 0).isActive = true
+        dataLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: 0).isActive = true
+        dataLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 0).isActive = true
+        dataLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: 0).isActive = true
+
+        contentView.addSubview(titleLabel)
+        titleLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 0).isActive = true
+        titleLabel.rightAnchor.constraint(equalTo: contentView.rightAnchor, constant: 0).isActive = true
+        titleLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -12).isActive = true
+        titleLabel.heightAnchor.constraint(equalToConstant: 21).isActive = true
     }
 }
+
