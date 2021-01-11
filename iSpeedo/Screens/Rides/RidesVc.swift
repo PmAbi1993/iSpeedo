83775046b63c78fed8996e6db6fc753e8b69397e
abhijith.pm <abhijith.pm@fingent.com>
2021-01-11 13:34:47 +0530

    Structure Creation

---
+++ iSpeedo/Screens/Rides/RidesVc.swift
@@ -0,0 +1,15 @@
+//
+//  RidesVc.swift
+//  iSpeedo
+//
+//  Created by admin on 11/01/21.
+//
+
+import UIKit
+
+class RidesVc: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ iSpeedo/Screens/Settings/SettingVC.swift
@@ -0,0 +1,29 @@
+//
+//  SettingVC.swift
+//  iSpeedo
+//
+//  Created by admin on 11/01/21.
+//
+
+import UIKit
+
+class SettingVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
